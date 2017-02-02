6db3ca587b0229df940839fb59d444819d452e6d
Zachary Jaros <Zachary.Jaros@asu.edu>
2017-02-02 06:14:02 -0700

    Add files via upload

---
+++ i3s.py
@@ -0,0 +1,3 @@
+# Intelligent Interactive Infotainment System I3S Neural Network
+# Sponsored By: Dr. Gaffar, Shokoufeh Monjezi, Seema Suresh
+# Development Team: Talab Hussein, Zach Jaros, Bin Hong Lee, Adam May, Miguel Zavala
